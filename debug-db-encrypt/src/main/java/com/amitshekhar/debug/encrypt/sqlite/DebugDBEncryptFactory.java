d1933b1e092fb802ae4465ed1c5894a8983125a6
Eliezer Graber <eygraber@gmail.com>
2020-01-14 01:36:41 -0500

    Use Relinker to load sqlcipher

--- debug-db-encrypt/src/main/java/com/amitshekhar/debug/encrypt/sqlite/DebugDBEncryptFactory.java
+++ debug-db-encrypt/src/main/java/com/amitshekhar/debug/encrypt/sqlite/DebugDBEncryptFactory.java
@@ -4,14 +4,21 @@ import android.content.Context;
 
 import com.amitshekhar.sqlite.DBFactory;
 import com.amitshekhar.sqlite.SQLiteDB;
+import com.getkeepsafe.relinker.ReLinker;
 
 import net.sqlcipher.database.SQLiteDatabase;
 
 public class DebugDBEncryptFactory implements DBFactory {
 
     @Override
-    public SQLiteDB create(Context context, String path, String password) {
-        SQLiteDatabase.loadLibs(context);
+    public SQLiteDB create(final Context context, String path, String password) {
+        SQLiteDatabase.loadLibs(context, new SQLiteDatabase.LibraryLoader() {
+            @Override public void loadLibraries(String... libNames) {
+                for (String libName : libNames) {
+                    ReLinker.loadLibrary(context, libName);
+                }
+            }
+        });
         return new DebugEncryptSQLiteDB(SQLiteDatabase.openOrCreateDatabase(path, password, null));
     }
 
